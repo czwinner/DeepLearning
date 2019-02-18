524f4490294b25fb1007204cb35d8a5034d60090
czwinner <46663760+czwinner@users.noreply.github.com>
2019-02-18 18:10:16 +0800

    Update contours.py

--- captcha_breaker/imutils/contours.py
+++ captcha_breaker/imutils/contours.py
@@ -23,19 +23,3 @@ def sort_contours(cnts, method="left-to-right"):
 
     # return the list of sorted contours and bounding boxes
     return cnts, boundingBoxes
-
-
-def label_contour(image, c, i, color=(0, 255, 0), thickness=2):
-    # compute the center of the contour area and draw a circle
-    # representing the center
-    M = cv2.moments(c)
-    cX = int(M["m10"] / M["m00"])
-    cY = int(M["m01"] / M["m00"])
-
-    # draw the contour and label number on the image
-    cv2.drawContours(image, [c], -1, color, thickness)
-    cv2.putText(image, "#{}".format(i + 1), (cX - 20, cY), cv2.FONT_HERSHEY_SIMPLEX,
-                1.0, (255, 255, 255), 2)
-
-    # return the image with the contour number drawn on it
-    return image
